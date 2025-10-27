d139edfbeea1280f5715ed17d5fd7a0ad5d8a655
electronerd92 <electronerd92@gmail.com>
2025-10-27 17:16:14 +0100

    Add header files for irrigation system components: WaterSourceManager, ValveManager, MainWaterSource, PumpTankSource, and Valve

---
+++ lib/IrrigationSystem/src/IrrigationSystem.h
@@ -0,0 +1,21 @@
+// TODO
+/*
+L'irrigation System ha
+    - un waterSource Manager
+    - un Valve Manager
+
+    Il waterSource Manager ha due sorgenti
+        - main water source
+        - pumpTank source
+
+    Deve
+    - assicurare che la source è available
+    - avviare fermare la source
+    - rimpire il tank
+
+    Il valve manager ha
+        - 8 valvole (4 ext e 4 int)
+        - deve verificare attivamente se ci sono valvole da aprire o chiudere
+        - deve poter aprire e chiudere valvole singolarmente
+
+*/
